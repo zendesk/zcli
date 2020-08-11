e75ffc988969cae57478adb3310c426869fc6919
Sarmad Sangi <sarmadsangi@gmail.com>
2020-08-11 14:21:27 +1000

    fix package functional test for windows

--- packages/zcli-apps/tests/functional/package.test.ts
+++ packages/zcli-apps/tests/functional/package.test.ts
@@ -2,6 +2,7 @@ import { expect, test } from '@oclif/test'
 import * as path from 'path'
 
 describe('package', function () {
+  const appPath = path.join(__dirname, 'mocks/single_product_app')
   test
     .env({
       ZENDESK_SUBDOMAIN: 'z3ntest',
@@ -14,9 +15,10 @@ describe('package', function () {
         .reply(200)
     })
     .stdout()
-    .command(['apps:package', path.join(__dirname, 'mocks/single_product_app')])
+    .command(['apps:package', appPath])
     .it('should display success message package is created', ctx => {
-      expect(ctx.stdout).to.contain('Package created at packages/zcli-apps/tests/functional/mocks/single_product_app/tmp/app')
+      const pkgPath = path.join(path.relative(process.cwd(), appPath), 'tmp', 'app')
+      expect(ctx.stdout).to.contain(`Package created at ${pkgPath}`)
     })
 
   test
