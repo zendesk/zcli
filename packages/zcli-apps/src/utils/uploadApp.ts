1fc8dc0ffde660ced67bbf6af8a20e7eb0a1d99e
Christopher Goddard <cgoddard@zendesk.com>
2021-10-22 10:44:37 +1100

    Extract update by product to utility function

--- packages/zcli-apps/src/utils/uploadApp.ts
+++ packages/zcli-apps/src/utils/uploadApp.ts
@@ -1,5 +1,7 @@
 import { setConfig } from '../utils/appConfig'
 import { request } from '@zendesk/zcli-core'
+import { getAppSettings } from './getAppSettings'
+import { Manifest, Installations, ZcliConfigFileContent } from '../types'
 
 export const getUploadJobStatus = async (job_id: string, appPath: string, pollAfter = 1000) => new Promise((resolve, reject) => {
   const polling = setInterval(async () => {
@@ -16,3 +18,25 @@ export const getUploadJobStatus = async (job_id: string, appPath: string, pollAf
     }
   }, pollAfter)
 })
+
+export const updateProductInstallation = async (appConfig: ZcliConfigFileContent, manifest: Manifest, app_id: string, product: string): Promise<boolean> => {
+  const installations: Installations = await request.requestAPI(`/api/${product}/apps/installations.json`, {}, true)
+
+  const configParams = appConfig?.parameters || {} // if there are no parameters in the config, just attach an empty object
+  const settings = manifest.parameters ? await getAppSettings(manifest, configParams) : {}
+  const installation_id = installations.installations.filter(i => i.app_id === app_id)[0].id
+
+  const updated = await request.requestAPI(`/api/${product}/apps/installations/${installation_id}.json`, {
+    method: 'PUT',
+    body: JSON.stringify({ settings: { name: manifest.name, ...settings } }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+
+  if (updated.status === 201 || updated.status === 200) {
+    return true
+  } else {
+    return false
+  }
+}
