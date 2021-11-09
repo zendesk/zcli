8319744eaea26f323ba336b486c9312649509517
Anushkafka <afernando@zendesk.com>
2021-11-09 12:08:32 +1100

    Add ssh key with write access

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,13 +6,7 @@ set -e
 echo '🔄 Generate tag, update docs and changelog'
 yarn install --frozen-lockfile
 
-./scripts/generate_dev_docs.sh
-
-# Investigate why these are generated
-git checkout -- docs/apps.md
-git checkout -- docs/autocomplete.md
-git checkout -- docs/login.md
-rm docs/commands.md
+# ./scripts/generate_dev_docs.sh
 
 # Remove beta once we are out of it
 npx lerna publish --conventional-commits --yes --preid 'beta' --no-verify-access
