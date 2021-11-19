5e3cd1cbff1cbbbaadd6007ef8d9788c1c767f88
william xing <etanxing@gmail.com>
2021-11-19 21:38:57 +1030

    [VEG-876] ZCLI: Parse subdomain even when hostname is provided

--- packages/zcli-core/src/lib/authUtils.ts
+++ packages/zcli-core/src/lib/authUtils.ts
@@ -1,15 +1,15 @@
 /**
- * Parse a subdomain. 
- * 
+ * Parse a subdomain.
+ *
  * If someone mistakenly provides full hostname or Url instead of a subdomain
  * then strip out domain name from it.
- * 
+ *
  * @param {string} subdomain - The subdomain.
  * @return {string} The parsed subdomain.
  */
 export const parseSubdomain = (subdomain: string) => {
   subdomain = subdomain.trim().toLowerCase()
-  const regex = /(?:http[s]*\:\/\/)*(.*?)\.zendesk.com$/i
+  const regex = /(?:http[s]*:\/\/)*(.*?)\.zendesk.com$/i
   const result = subdomain.match(regex)
   return result !== null ? result[1] : subdomain
 }
