d0640bcac927c17cc5484631445b5863b515e22b
william xing <etanxing@gmail.com>
2021-11-22 09:56:55 +1030

    [VEG-876] Updated "forward slash" cases

--- packages/zcli-core/src/lib/authUtils.test.ts
+++ packages/zcli-core/src/lib/authUtils.test.ts
@@ -5,8 +5,8 @@ describe('authUtils', () => {
   describe('parseSubdomain', () => {
     test
       .it('should extract the subdomain from a url', async () => {
-        expect(parseSubdomain('https://Test1.zeNDEsk.com')).to.equal('test1')
-        expect(parseSubdomain(' hTTp://tEst2.zendesk.com')).to.equal('test2')
+        expect(parseSubdomain('https://Test1.zeNDEsk.com/')).to.equal('test1')
+        expect(parseSubdomain(' hTTp://tEst2.zendesk.com/ ')).to.equal('test2')
         expect(parseSubdomain('test3.zendesk.com ')).to.equal('test3')
         expect(parseSubdomain('test4')).to.equal('test4')
       })
