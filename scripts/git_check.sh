119647d8226e2fdb6c86f31137980275f96989aa
Christopher Goddard <cgoddard@zendesk.com>
2022-05-04 09:51:06 +1000

    Add comments to git_check to explain its purpose

--- scripts/git_check.sh
+++ scripts/git_check.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# git_check determines whether there are any uncommitted changes
+# the key purpose of this check is to run after `yarn install` in the test GA workflow
+# so as to confirm that `yarn install` does not cause a delta in any of the lock files
+
 delta=$(git status -s | wc -c | awk '{$1=$1};1')
 if((delta > 0)); then
     echo "There are uncommited changes."
