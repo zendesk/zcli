323e150c511fcdb81c65636ea63badcd89c56087
william xing <william.xing@zendesk.com>
2022-11-08 13:29:08 +1030

    [VEG-742] Reversed yarn.lock change

--- scripts/git_check.sh
+++ scripts/git_check.sh
@@ -3,7 +3,6 @@
 # git_check determines whether there are any uncommitted changes
 # the key purpose of this check is to run after `yarn install` in the test GA workflow
 # so as to confirm that `yarn install` does not cause a delta in any of the lock files
-git status -s
 
 delta=$(git status -s | wc -c | awk '{$1=$1};1')
 if((delta > 0)); then
