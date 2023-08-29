9dfaf0e8ec0e472a8d0b6fb6113f317c76126c2f
JeanMarcGoepfert <jgoepfert@zendesk.com>
2023-08-29 16:56:19 +1000

    Switch script to axios

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-const axios = require("axios");
+const fetch = require("node-fetch");
 const fs = require('fs');
 
 const subdomain = process.env.ZENDESK_SUBDOMAIN;
@@ -6,19 +6,17 @@ const email = process.env.ZENDESK_EMAIL;
 const password = process.env.ZENDESK_PASSWORD;
 const creds = btoa(email + ':' + password);
 
-const config = {
-  method: "GET",
-  url: `https://${subdomain}.zendesk.com/api/v2/apps/owned.json`,
-  headers: {
-    "Authorization": `Basic ${creds}`,
-  },
-};
-
-
-axios(config)
-  .then(function (response) {
-    console.log(JSON.stringify(response.data));
-  })
-  .catch(function (error) {
-    console.log(error)
+async function main() {
+  const response = await fetch(`https://${subdomain}.zendesk.com/api/v2/account/settings.json`, {
+    method: 'GET',
+    headers: new Headers({
+      'Authorization': 'Basic '+creds,
+    })
   });
+
+  const data = await response.json();
+
+  console.log(data);
+}
+
+main();
