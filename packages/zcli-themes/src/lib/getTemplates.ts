9a4da9c5dcd07e2e7016d78d9842cf003d86bcc4
Sébastien Warin <sebastien@warin.fr>
2024-01-18 23:42:01 +0100

    fix: convert Windows paths to POSIX format

--- packages/zcli-themes/src/lib/getTemplates.ts
+++ packages/zcli-themes/src/lib/getTemplates.ts
@@ -3,10 +3,10 @@ import * as fs from 'fs'
 
 export default function getTemplates (themePath: string): Record<string, string> {
   const templates: Record<string, string> = {}
-  const filenames = globSync(`${themePath}/templates/**/*.hbs`)
+  const filenames = globSync(`${themePath}/templates/**/*.hbs`.replace(/\\/g, '/'))
 
   filenames.forEach((template) => {
-    const identifier = template.split('templates/')[1].split('.hbs')[0]
+    const identifier = template.replace(/\\/g, '/').split('templates/')[1].split('.hbs')[0]
     const source = fs.readFileSync(template, 'utf8')
     templates[identifier] = source
   })
