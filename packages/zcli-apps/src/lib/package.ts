cd5af49ae61cd78266451b9004800da0be398afb
anushkafka <anushka.rf@gmail.com>
2024-09-03 10:52:56 +1000

    Fix some lints

--- packages/zcli-apps/src/lib/package.ts
+++ packages/zcli-apps/src/lib/package.ts
@@ -54,7 +54,6 @@ export const createAppPkg = (
 }
 
 export const validatePkg = async (pkgPath: string) => {
-
   if (!fs.pathExistsSync(pkgPath)) {
     throw new CLIError(`Package not found at ${pkgPath}`)
   }
