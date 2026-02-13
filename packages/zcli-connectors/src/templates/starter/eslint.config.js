7c925dcc1d7d0363f6aa6952ff1fd0404d4ddca7
Peter Nguyen <peter.nguyen@zendesk.com>
2026-02-13 12:21:05 +1100

    fix: connectors template scaffold to use versions of its dependencies in package.json and correctly valid typescript config for connectors

--- packages/zcli-connectors/src/templates/starter/eslint.config.js
+++ packages/zcli-connectors/src/templates/starter/eslint.config.js
@@ -1,3 +1,48 @@
-import { config } from '@connectors/eslint-config';
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import tseslint from 'typescript-eslint';
 
-export default config({ typescript: true });
+export default [
+  js.configs.recommended,
+  eslintConfigPrettier,
+  {
+    rules: {
+      'no-console': process.env.CI ? 'error' : 'warn',
+    },
+  },
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+      },
+    },
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          ignoreRestSiblings: true,
+        },
+      ],
+      '@typescript-eslint/prefer-nullish-coalescing': [
+        'error',
+        {
+          ignorePrimitives: {
+            string: true,
+            boolean: true,
+          },
+          ignoreMixedLogicalExpressions: true,
+          ignoreConditionalTests: true,
+        },
+      ],
+      '@typescript-eslint/prefer-optional-chain': ['error', { checkString: false }],
+      '@typescript-eslint/no-unnecessary-condition': 'error',
+      '@typescript-eslint/no-unnecessary-type-assertion': 'error',
+    },
+  },
+  {
+    ignores: ['dist/**'],
+  },
+];
