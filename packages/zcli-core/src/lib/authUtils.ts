e29c95e01c6db13b2a9fdab090c6ed9208776497
Michael Tsolakis <mtsolakis@zendesk.com>
2026-08-06 12:35:47 +1000

    improve polynomial regular expression

--- packages/zcli-core/src/lib/authUtils.ts
+++ packages/zcli-core/src/lib/authUtils.ts
@@ -11,7 +11,7 @@ import { Profile } from '../types'
  */
 export const parseSubdomain = (subdomain: string) => {
   subdomain = subdomain.trim().toLowerCase()
-  const regex = /(?:http[s]*:\/\/)*(.*?)\.zendesk.com[/]?$/i
+  const regex = /^(?:https?:\/\/)?([^/.]+)\.zendesk\.com(?:\/.*)?$/i
   const result = regex.exec(subdomain)
   return result !== null ? result[1] : subdomain
 }
